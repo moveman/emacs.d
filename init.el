8abce6bd1eb16e6a79310e5ff4c33fd19cb7185b
leon <leon.lyy@gmail.com>
2017-08-03 08:39:38 +0800

    wip

--- init.el
+++ init.el
@@ -1,17 +1,3 @@
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/core/")
-
-(require 'core-booting)
-(require 'core-builtin)
-(require 'core-editing)
-(require 'core-windows)
-
-(require 'core-c++)
-(require 'core-clojure)
-(require 'core-emacs-lisp)
-(require 'core-go)
-(require 'core-python)
-(require 'core-rust)
-
 (org-babel-load-file "~/.emacs.d/core.org")
